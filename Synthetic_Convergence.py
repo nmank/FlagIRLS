15f558bc14d8f810f2c1ee7243dc39cb447d50c2
Nathan Mankovich <mankovic@kick.katrina.colostate.edu>
2021-08-31 14:10:41 -0600

    adding test and new code

---
+++ Synthetic_Convergence.py
@@ -0,0 +1,107 @@
+import numpy as np
+import center_algorithms as ca
+import matplotlib.pyplot as plt
+
+
+'''
+plotting function
+'''
+def add_line(data, lstyle, mkr, lbl, color = 'b', ci = 95):
+    med = np.median(data, axis = 0)
+
+    lower_ci = []
+    upper_ci = []
+    for i in range(data.shape[1]):
+        lower_ci.append(np.percentile(data[:,i],50-ci/2))
+        upper_ci.append(np.percentile(data[:,i],50+ci/2))
+    plt.fill_between(list(np.arange(len(med))), lower_ci, upper_ci, alpha=0.25, color = color)
+    plt.xticks(np.arange(0, len(med), 2))
+    # plt.rcParams["text.usetex"] =True
+    plt.plot(med, color = color, linestyle=lstyle, marker=mkr, markevery = 2, linewidth=.5, label = lbl)
+
+
+
+
+def convergence_check(gr_list, n_its):
+
+    [n,k] = gr_list[0].shape
+
+    irls_sin_median = []
+    gd_sin_median = []
+
+    # irls_geodesic_median = []
+    # gd_geodesic_median = []
+
+    irls_max_cosine = []
+    gd_max_cosine = []
+
+    Y_raw = np.random.rand(n,k)
+    Y = np.linalg.qr(Y_raw)[0][:,:k]
+
+    for i in range(10): 
+
+        irls_sin_median.append(ca.irls_flag(gr_list, k, n_its, 'sine', fast = False, init = Y)[1])
+        gd_sin_median.append(ca.gradient_descent(gr_list, k, -.01, n_its, 'sine', init = Y)[1])
+        print('Sine Median finished')
+
+        # irls_geodesic_median.append(ca.irls_flag(gr_list, k, n_its, 'geodesic', fast = False, init = Y)[1])
+        # gd_geodesic_median.append(ca.gradient_descent(gr_list, k, .01, n_its, 'geodesic', init = Y)[1])
+        # print('Geodesic finished')
+
+        irls_max_cosine.append(ca.irls_flag(gr_list, k, n_its, 'cosine', fast = False, init = Y)[1])
+        gd_max_cosine.append(ca.gradient_descent(gr_list, k, -.01, n_its, 'cosine', init = Y)[1])
+        print('Max Cos finished')
+
+    irls_sin_median = np.vstack(irls_sin_median)
+    gd_sin_median = np.vstack(gd_sin_median)
+
+    # irls_geodesic_median = np.vstack(irls_geodesic_median)
+    # gd_geodesic_median = np.vstack(gd_geodesic_median)
+
+    irls_max_cosine = np.vstack(irls_max_cosine)
+    gd_max_cosine = np.vstack(gd_max_cosine)
+
+    #make the plots
+    LINESTYLES = ["-", "--", ":", "-."]
+    MARKERS = ['D', 'o', 'X', '*', '<', 'd', 'S', '>', 's', 'v']
+    COLORS = ['b','k','c','m','y']
+
+    add_line(irls_sin_median, LINESTYLES[0], MARKERS[0], 'IRLS', 'g')
+    add_line(gd_sin_median, LINESTYLES[1], MARKERS[1], 'Gradient Descent', 'b')
+    plt.legend()
+    plt.xlabel('Iteration')
+    plt.ylabel('Objective function value')
+    plt.savefig('./Figures/sin_median_convergence.png')
+    plt.close()
+
+    # add_line(irls_geodesic_median, LINESTYLES[0], MARKERS[0], 'IRLS', 'g')
+    # add_line(gd_geodesic_median, LINESTYLES[1], MARKERS[1], 'Gradient Descent', 'b')
+    # plt.legend()
+    # plt.savefig('./Figures/geodesic_median_convergence.png')
+    # plt.close()
+
+    add_line(irls_max_cosine, LINESTYLES[0], MARKERS[0], 'IRLS', 'g')
+    add_line(gd_max_cosine, LINESTYLES[1], MARKERS[1], 'Gradient Descent', 'b')
+    plt.legend()
+    plt.xlabel('Iteration')
+    plt.ylabel('Objective function value')
+    plt.savefig('./Figures/max_cosine_convergence.png')
+    plt.close()
+
+
+
+n = 100
+r = 3
+m = 10
+
+n_its = 20
+
+data = []
+#sample from uniform distribution
+for i in range(m):
+	[u,t] = np.linalg.qr(np.random.rand(n,n))
+	data.append(u[:,:r])
+
+
+
+convergence_check(data, n_its)
---
+++ center_algorithms.py
@@ -0,0 +1,217 @@
+import numpy as np
+
+'''
+- fix PLS code for fast == True
+- horst's and maybe one more algorith
+- 2d checks
+'''
+
+
+def calc_error_cos(data, Y):
+    s = np.linalg.svd(Y.T @ data)[1]
+    return np.sum(s)
+
+def calc_error_1_2(data, Y, sin_cos):
+    k = Y.shape[1]
+    err = 0
+    if sin_cos == 'cosine':
+        for x in data:
+            r = np.min([k,x.shape[1]])
+            err += np.sqrt(np.trace(Y.T @ x @ x.T @ Y))
+    elif sin_cos == 'sine':
+        for x in data:
+            r = np.min([k,x.shape[1]])
+            sin_sq = r - np.trace(Y.T @ x @ x.T @ Y)
+            if sin_sq < 0:
+                sin_sq = 0
+            err += np.sqrt(sin_sq)
+    elif sin_cos == 'geodesic':
+        for x in data:
+            cos = np.sqrt(Y.T @ x @ x.T @ Y)[0][0]
+            if cos > 1:
+                print(cos)
+                cos= 1
+            elif cos < 0:
+                print(cos)
+                cos = 0
+            err += np.arccos(cos)
+    return err
+
+
+def flag_mean(data, k, fast = False):
+    X = np.hstack(data)
+    if fast:
+        #Initialize
+        # U  = X[:,:k][:] 
+        mean = np.random.rand(X.shape[0],k)
+        V = np.zeros((X.shape[1],k))
+
+        #err[j] stores 2-norm squared difference between ith and (i+1)th iterate of U[:,j]
+        err = []
+        #iteration of partial least squares
+        for j in range(k):
+            err1 = []
+            err1.append(1)
+            while err1[-1] > .000000001:
+            # for _ in range(n_iters):
+                old_U = mean[:,[j]][:]
+                V[:,j] = mean[:,j] @ X / (mean[:,j] @ mean[:,[j]])
+                V[:,j] = V[:,j]/np.linalg.norm(V[:,j])
+                new_U =  X @ V[:,[j]] / (V[:,j] @ V[:,[j]])
+                err1.append(np.linalg.norm(old_U - new_U)**2)
+                print(err1[-1])
+                mean[:,[j]] = new_U[:]
+            err.append(err1[1:])
+        
+    else:
+        mean = np.linalg.svd(X)[0][:,:k]
+    return mean
+
+def flag_mean_iteration(data, Y0, weight, fast = False):
+    k = Y0.shape[1]
+    aX = []
+    al = []
+    converge_data_point = False
+
+    ii=0
+
+    for x in data:
+        if weight == 'cosine':
+            al.append(np.trace(Y0.T @ x @ x.T @ Y0)**(-1/4))
+        elif weight == 'sine':
+            r = np.min([k,x.shape[1]])
+            sin_sq = r - np.trace(Y0.T @ x @ x.T @ Y0)
+            if sin_sq < .0000000001:
+                converge_data_point = True
+                converge_data_point_idx = ii
+                al.append(1)
+                break
+            else:
+                al.append(sin_sq**(-1/4))
+        elif weight == 'geodesic':
+            r = np.min([k,x.shape[1]])
+            sin_sq = 1 - Y0.T @ x @ x.T @ Y0
+            if sin_sq < .0000000001:
+                converge_data_point = True
+                converge_data_point_idx = ii
+                al.append(1)
+                break
+            else:
+                al.append(((sin_sq)**(-1/4))*((Y0.T @ x @ x.T @ Y0)**(-1/4)))
+        else:
+            print('sin_cos must be geodesic, sine or cosine')
+        aX.append(al[-1]*x)
+        ii+= 1
+
+    if converge_data_point:
+        Y0 = data[converge_data_point_idx] 
+    else:
+        Y0 = flag_mean(aX, k, fast)
+
+    return Y0
+
+def irls_flag(data, k, n_its, sin_cos, fast = False, init = 'random'): 
+    err = []
+    n = data[0].shape[0]
+
+
+    #initialize
+    if init == 'random':
+        #randomly
+        Y_raw = np.random.rand(n,k)
+        Y = np.linalg.qr(Y_raw)[0][:,:k]
+    else:
+        Y = init
+
+    err.append(calc_error_1_2(data, Y, sin_cos))
+
+    #flag mean iteration function
+    for _ in range(n_its):
+        Y = flag_mean_iteration(data, Y, sin_cos, fast)
+        err.append(calc_error_1_2(data, Y, sin_cos))
+
+    return Y, err
+
+
+#This is wrong!!!
+# def irls_flag_1_infty_cosine(data, k, n_its):
+#     n = data[0].shape[0]
+
+#     #initialize
+#     #randomly
+#     Y_raw = np.random.rand(n,k)
+#     Y = np.linalg.qr(Y_raw)[0][:,:k]
+
+#     Beta = []
+    
+#     for x in data:
+#         for i in range(n_its):
+#             w = np.trace(Y.T @ x @ x.T @ Y)**(-1/4)
+#             B = np.linalg.svd(w*x)[0][:,[0]]
+#         Beta.append(B)
+#     Beta = np.hstack(Beta)
+
+#     Y = np.linalg.svd(Beta)[0][:,:k]
+#     return Y
+
+def calc_gradient(data, Y0, weight):
+    k = Y0.shape[1]
+    aX = []
+    al = []
+    for x in data:
+        if weight == 'cosine':
+            al.append(np.trace(Y0.T @ x @ x.T @ Y0)**(-1/4))
+        elif weight == 'sine':
+            r = np.min([k,x.shape[1]])
+            sin_sq = r - np.trace(Y0.T @ x @ x.T @ Y0)
+            if sin_sq < .0000000001 :
+                sin_sq = 0
+            al.append(sin_sq**(-1/4))
+        elif weight == 'geodesic':
+            r = np.min([k,x.shape[1]])
+            al.append(((1 - Y0.T @ x @ x.T @ Y0)**(-1/4))*((Y0.T @ x @ x.T @ Y0)**(-1/4)))
+        else:
+            print('sin_cos must be geodesic, sine or cosine')
+        aX.append(al[-1]*x)
+
+    big_X = np.hstack(aX)
+
+    return big_X @ big_X.T @ Y0
+
+
+def gradient_descent(data, k, alpha, n_its, sin_cos, init = 'random'):
+    n = data[0].shape[0]
+
+    #initialize
+    if init == 'random':
+        #randomly
+        Y_raw = np.random.rand(n,k)
+        Y = np.linalg.qr(Y_raw)[0][:,:k]
+    else:
+        Y = init
+
+    err = []
+    err.append(calc_error_1_2(data, Y, sin_cos))
+
+    for _ in range(n_its):
+        Fy = calc_gradient(data,Y,sin_cos)
+        # project the gradient onto the tangent space
+        G = (np.eye(n)-Y@Y.T)@Fy
+        # %error check
+        # if sum(G) == 0 || any(isnan(G))
+        #     Fy
+        #     G
+        #     break
+        # end
+        # move to a point that is alpha along the geodesic at Y0 in the
+        # direction of G
+        [U,S,V] = np.linalg.svd(G)
+        cosin = np.diag(np.cos(-alpha*S))
+        sin = np.vstack([np.diag(np.sin(-alpha*S)), np.zeros((n-k,k))])
+        if cosin.shape[0] == 1:
+            Y = Y*V*cosin*V.T+U@sin *V.T
+        else:
+            Y = Y@V@cosin@V.T+U@sin@V.T
+        
+        err.append(calc_error_1_2(data, Y, sin_cos))
+    return Y, err
